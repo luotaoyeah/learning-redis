890bd80ff1be4486b21961c215f10c6bc445ee7c
luotao <luotaoyeah@gmail.com>
2024-07-24 01:18:48 +0800

    feat(2022-itheima): HGET

--- projects/2022-itheima/src/基础篇/11-hash.sql
+++ projects/2022-itheima/src/基础篇/11-hash.sql
@@ -3,10 +3,22 @@
 -- https://redis.io/docs/latest/commands/hset/
 -- hash 类型的数据, 就类似于 java 中的 HashMap, 是一组无序的 field/value 对,
 
--- 设置某个 KEY 中的一个 field/value 对,
+-- 设置某个 key 中的一个 field/value 对,
 HSET user:3 name LUOTAO
 DEL user:3
 
--- 设置某个 KEY 中的多个 field/value 对,
+-- 设置某个 key 中的多个 field/value 对,
 HSET user:3 name LUOTAO age 18
+DEL user:3
+
+
+-- HGET
+-- ----------------------------------------------------------------------------------------------------
+-- https://redis.io/docs/latest/commands/hget/
+-- hash 类型的数据, 就类似于 java 中的 HashMap, 是一组无序的 field/value 对,
+
+-- 获取某个 key 下的某个 field 对应的 value,
+HSET user:3 name LUOTAO age 18
+HGET user:3 name
+HGET user:3 age
 DEL user:3
