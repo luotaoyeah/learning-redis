d5df5db1538e930ede174f22cf0f03513f06c52f
luotao <luotaoyeah@gmail.com>
2024-07-24 02:46:33 +0800

    feat(2022-itheima): SMEMBERS

--- projects/2022-itheima/src/基础篇/11-set.sql
+++ projects/2022-itheima/src/基础篇/11-set.sql
@@ -1,20 +1,31 @@
 -- SADD
 -- ----------------------------------------------------------------------------------------------------
 -- https://redis.io/docs/latest/commands/sadd/
--- 往集合中添加一个元素
+-- 往集合中添加元素
 
 SADD set:01 A B C
 SMEMBERS set:01
 DEL set:01
 
+
 -- SREM
 -- ----------------------------------------------------------------------------------------------------
 -- https://redis.io/docs/latest/commands/srem/
--- 往集合中添加一个元素
+-- 从集合中删除一个元素
 
 SADD set:01 A B C
 SMEMBERS set:01
 SREM set:01 B
 SMEMBERS set:01
+SREM set:01 A C
 DEL set:01
 
+
+-- SMEMBERS
+-- ----------------------------------------------------------------------------------------------------
+-- https://redis.io/docs/latest/commands/smembers/
+-- 返回集合中的所有元素
+
+SADD set:01 A B C
+SMEMBERS set:01
+DEL set:01
