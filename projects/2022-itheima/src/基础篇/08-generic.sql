dcd6fadb53f1ada0e0436a535f028489db4ec88c
luotao <luotaoyeah@gmail.com>
2024-07-24 00:17:22 +0800

    feat(2022-itheima): escape special characters

--- projects/2022-itheima/src/基础篇/08-generic.sql
+++ projects/2022-itheima/src/基础篇/08-generic.sql
@@ -1,12 +1,14 @@
--- KEYS 模糊查询 KEY
--- https://redis.io/docs/latest/commands/keys/
+-- KEYS
 -- ----------------------------------------------------------------------------------------------------
+-- https://redis.io/docs/latest/commands/keys/
+-- 模糊查询 KEY
 
 KEYS *
 
--- DEL 删除一个或多个 KEY, 返回被删除的 KEY 的数量,
--- https://redis.io/docs/latest/commands/del/
+-- DEL
 -- ----------------------------------------------------------------------------------------------------
+-- https://redis.io/docs/latest/commands/del/
+-- 删除一个或多个 KEY, 返回被删除的 KEY 的数量,
 
 SET foo "FOO"
 SET bar "BAR"
@@ -20,16 +22,18 @@ DEL foo bar
 DEL TEST
 
 
--- EXISTS 判断某个 KEY 是否存在,
--- https://redis.io/docs/latest/commands/expire/
+-- EXISTS
 -- ----------------------------------------------------------------------------------------------------
+-- https://redis.io/docs/latest/commands/expire/
+-- 判断某个 KEY 是否存在,
 
 EXISTS TEST
 
 
--- TTL 查看某个 KEY 的剩余存活时间, 单位为秒,
--- https://redis.io/docs/latest/commands/ttl/
+-- TTL
 -- ----------------------------------------------------------------------------------------------------
+-- https://redis.io/docs/latest/commands/ttl/
+-- 查看某个 KEY 的剩余存活时间, 单位为秒,
 
 -- -- 返回值 -2 表示该 KEY 不存在,
 TTL bar
@@ -40,9 +44,10 @@ TTL foo
 DEL foo
 
 
--- EXPIRE 设置某个 KEY 的有效时间, 过期之后该 KEY 会被自动删除, 单位为秒,
--- https://redis.io/docs/latest/commands/expire/
+-- EXPIRE
 -- ----------------------------------------------------------------------------------------------------
+-- https://redis.io/docs/latest/commands/expire/
+-- 设置某个 KEY 的有效时间, 过期之后该 KEY 会被自动删除, 单位为秒,
 SET foo "FOO"
 EXPIRE foo 10
 TTL foo
