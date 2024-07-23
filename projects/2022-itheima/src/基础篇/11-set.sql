c19bd6308e15d46684409435229db8943370ef35
luotao <luotaoyeah@gmail.com>
2024-07-24 02:45:07 +0800

    feat(2022-itheima): SREM

--- projects/2022-itheima/src/基础篇/11-set.sql
+++ projects/2022-itheima/src/基础篇/11-set.sql
@@ -5,3 +5,16 @@
 
 SADD set:01 A B C
 SMEMBERS set:01
+DEL set:01
+
+-- SREM
+-- ----------------------------------------------------------------------------------------------------
+-- https://redis.io/docs/latest/commands/srem/
+-- 往集合中添加一个元素
+
+SADD set:01 A B C
+SMEMBERS set:01
+SREM set:01 B
+SMEMBERS set:01
+DEL set:01
+
