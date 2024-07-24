aa90a96bcfef7ef29c6d820e05720eefcb8aaa1c
luotao <luotaoyeah@gmail.com>
2024-07-25 01:11:21 +0800

    feat(2022-itheima): jedis: set/get/del/hset/hgetAll

--- projects/2022-itheima/src/test/java/com/luotao/learningredis/_16.java
+++ projects/2022-itheima/src/test/java/com/luotao/learningredis/_16.java
@@ -3,6 +3,9 @@ package com.luotao.learningredis;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class _16 extends _00 {
 
     @Test
@@ -15,5 +18,24 @@ public class _16 extends _00 {
 
         // DEL
         jedis.del("a");
+    }
+
+    @Test
+    public void _02() {
+        jedis.del("hash:01");
+
+        // HSET
+        Assertions.assertEquals(1, jedis.hset("hash:01", "name", "LUOTAO"));
+
+        Map<String, String> map = new HashMap<>();
+        map.put("age", "18");
+        Assertions.assertEquals(1, jedis.hset("hash:01", map));
+
+        // HGETALL
+        Map<String, String> result = jedis.hgetAll("hash:01");
+        Assertions.assertEquals("LUOTAO", result.get("name"));
+        Assertions.assertEquals("18", result.get("age"));
+
+        jedis.del("hash:01");
     }
 }
