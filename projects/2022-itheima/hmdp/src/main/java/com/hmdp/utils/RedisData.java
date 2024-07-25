6e0c8d4c102e76a89a10f55af3aeccb7435e90d4
luotao <luotaoyeah@gmail.com>
2024-07-26 01:11:45 +0800

    feat(2022-itheima): import project dianping

---
+++ projects/2022-itheima/hmdp/src/main/java/com/hmdp/utils/RedisData.java
@@ -0,0 +1,12 @@
+package com.hmdp.utils;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class RedisData {
+
+    private LocalDateTime expireTime;
+    private Object data;
+}
